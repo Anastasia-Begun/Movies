08741afb54994f69c657ff9a5cf626178b8f38ca
Begun Anastasiia <begun0anastasia@yandex.ru>
2025-04-09 17:18:28 +0300

    Загружаю и отображаю movies и TV-шоу на странице

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 import { global } from "./global.js";
+import { getData } from "./api/getData.js";
+import { generateTemplate } from "../js/utils/generateTemplate.js";
+import { tabsComponent } from "./components/tabs.js";
+// import { search } from "./api/searchServices.js";
 
 /**
  * Инициализирует функции в зависимости от страницы.
@@ -9,18 +13,47 @@ function init() {
     case "/":
     case "/index.html":
       // Вызываем функции для отображения фильмов в прокате (слайдер), а также популярных фильмов и сериалов
+      processFilmsAndShowsData("movie/now_playing");
+      processFilmsAndShowsData("movie/popular");
+      processFilmsAndShowsData("tv/popular");
+      tabsComponent();
 
       break;
-    case "/movie-details.html":
-      // Вызываем функцию для отображения деталей о фильме
-      break;
-    case "/tv-details.html":
-      // Вызываем функцию для отображения деталей о сериале
-      break;
-    case "/search.html":
-      // Вызываем функцию для выполнения поиска
-      break;
+    // case "/search.html":
+    //   // Вызываем функцию для выполнения поиска
+    //   search();
+    //   break;
   }
 }
 
 document.addEventListener("DOMContentLoaded", init);
+
+/**
+ * Функция для обработки данных о фильмах и сериалах.
+ *
+ * @param {string} endpoint - Конечная точка API.
+ */
+export const processFilmsAndShowsData = async (endpoint) => {
+  const { results } = await getData(endpoint);
+
+  // Определяем контейнер в зависимости от эндпоинта
+  let containerSelector;
+
+  switch (endpoint) {
+    case "movie/now_playing":
+      containerSelector = ".swiper-wrapper";
+      break;
+    case "movie/popular":
+      containerSelector = ".popular-movies";
+      break;
+    default:
+      containerSelector = ".popular-tv";
+  }
+
+  // Генерируем шаблон
+  generateTemplate(results, {
+    containerSelector,
+    // слайдер применяется только, если endpoint === "movie/now_playing"
+    useSlider: endpoint === "movie/now_playing",
+  });
+};
